ecedabd118468692c808f9d902cabd008693a1b1
xhzy-jiangyun <v-jiangyun@shuwen.com>
2023-11-09 10:34:33 +0800

    🐞 fix(ConnectWalletDialog): 删除组件

--- src/components/ConnectWallet/ConnectWalletDialog.tsx
+++ src/components/ConnectWallet/ConnectWalletDialog.tsx
@@ -1,9 +1,9 @@
-import CloseIcon from '@mui/icons-material/Close'
-import { Dialog, DialogContent, DialogTitle, IconButton } from '@mui/material'
+// import CloseIcon from '@mui/icons-material/Close'
+// import { Dialog, DialogContent, DialogTitle, IconButton } from '@mui/material'
 
 // import IconMetaMask from 'assets/icon-metamask.png'
-import ConnectWalletDialogButton from 'components/ConnectWallet/ConnectWalletDialogButton'
-import { injected } from 'components/Wallet/Connectors'
+// import ConnectWalletDialogButton from 'components/ConnectWallet/ConnectWalletDialogButton'
+// import { injected } from 'components/Wallet/Connectors'
 
 import type { SxProps } from '@mui/material'
 import type { AbstractConnector } from '@web3-react/abstract-connector'
@@ -22,21 +22,22 @@ const ConnectWalletDialog: React.FC<Props> = ({
   sx = {},
 }) => {
   return (
-    <Dialog fullWidth={true} onClose={handleClose} open={open}>
-      <DialogTitle>Connect wallet</DialogTitle>
-      <DialogContent>
-        <ConnectWalletDialogButton
-          onClick={() => handleConnect(injected)}
-          subtitle="Connect using MetaMask"
-          title="MetaMask"
-          // imgSrc={IconMetaMask}
-        />
-      </DialogContent>
+    <></>
+    // <Dialog fullWidth={true} onClose={handleClose} open={open}>
+    //   <DialogTitle>Connect wallet</DialogTitle>
+    //   <DialogContent>
+    //     <ConnectWalletDialogButton
+    //       onClick={() => handleConnect(injected)}
+    //       subtitle="Connect using MetaMask"
+    //       title="MetaMask"
+    //       imgSrc={IconMetaMask}
+    //     />
+    //   </DialogContent>
 
-      <IconButton className="absolute right-3 top-3" onClick={handleClose}>
-        <CloseIcon />
-      </IconButton>
-    </Dialog>
+    //   <IconButton className="absolute right-3 top-3" onClick={handleClose}>
+    //     <CloseIcon />
+    //   </IconButton>
+    // </Dialog>
   )
 }
 
